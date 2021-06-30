9520fa5821f7092648f76de3557a4f6300d914e2
Reugito <raosahebdhotreedu@gmail.com>
2021-06-30 17:13:10 +0530

    Adding Person class and its properties

---
+++ java_Day9_AddressBookSystem/src/com/day9/AddressBook/Person.java
@@ -0,0 +1,77 @@
+package com.day9.AddressBook;
+
+public class Person {
+	private String firstName, lastName, address, city, state, email;
+	private int zip, phoneNumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return (firstName+lastName)+": "+"firstName = " + firstName + ", lastName = " + lastName + ", address = " + address
+				+ ", city = " + city + ", state = " + state + ", email = " + email + ", zip = " + zip + ", phoneNumber = "
+				+ phoneNumber ;
+	}
+}
