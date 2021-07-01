778998bd0c1df6b0827937491b3cf4e171825097
Reugito <raosahebdhotreedu@gmail.com>
2021-07-01 19:19:03 +0530

    Adding menu to add, update, get and remove details

--- java_Day9_AddressBookSystem/src/com/day9/AddressBook/AddressBookManager.java
+++ java_Day9_AddressBookSystem/src/com/day9/AddressBook/AddressBookManager.java
@@ -1,8 +1,6 @@
 package com.day9.AddressBook;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBookManager {
 	List<Person> addressBookList = new ArrayList();
@@ -20,36 +18,118 @@ public class AddressBookManager {
 	void addPersonDetails(){
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Enter firstName: ");
+		System.out.print("Enter firsasdatName: ");
 		String firstName = sc.next();
-		System.out.print("\nEnter lastName: ");
+		System.out.print("Enter lastName: ");
 		String lastName = sc.next();
-		System.out.println("\nEnter address: ");
+		System.out.print("Enter address: ");
 		String address = sc.next();
-		System.out.println("\nEnter city: ");
+		System.out.print("Enter city: ");
 		String city = sc.next();
-		System.out.println("\nEnter state: ");
+		System.out.print("Enter state: ");
 		String state = sc.next();
-		System.out.println("\nEnter email: ");
+		System.out.print("Enter email: ");
 		String email = sc.next();
-		System.out.println("\nEnter zip: ");
-		String zip = sc.next();
-		System.out.println("\nEnter phoneNumber: ");
-		String phoneNumber = sc.next();
+		System.out.print("Enter zip: ");
+		int zip = sc.nextInt();
+		System.out.print("Enter phoneNumber: ");
+		int phoneNumber = sc.nextInt();
+
 		
 		Person person = new Person();
 		person.setFirstName(firstName);
 		person.setLastName(lastName);
-		person.setFirstName(address);
-		person.setLastName(city);
-		person.setFirstName(state);
-		person.setLastName(email);
-		person.setFirstName(zip);
-		person.setLastName(phoneNumber);
+		person.setAddress(address);
+		person.setCity(city);
+		person.setState(state);
+		person.setEmail(email);
+		person.setZip(zip);
+		person.setPhoneNumber(phoneNumber);
 		addPerson(person);
 	}
 	
-	void removeItem(Person address) {
-		addressBookList.remove(address);
+	void removePersonDetails() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter person name: ");
+		String name = sc.next();
+		Person person = getperson(name);
+		addressBookList.remove(person);
+		System.out.print("Deleted contact = "+person);
+	}
+	
+	Person getperson(String name) {
+		Iterator iterator = addressBookList.iterator();
+		for(Person elements : addressBookList ) {
+			if (iterator.hasNext()) {
+				Person item = (Person) iterator.next();
+				if(item.getFirstName().equalsIgnoreCase(name)) {
+					return item;
+				}
+			}		
+		}
+		return null;	
+	}
+	
+	void updatePersonDetails() {
+		
+		System.out.print("Enter person entity: ");
+		Scanner sc = new Scanner(System.in);
+		String item = sc.next();
+		Person bookitem = getperson(item);
+		System.out.println(bookitem);
+		
+		System.out.println("1. update firstname");
+		System.out.println("2. update Lastname");
+		System.out.println("3. update address");
+		System.out.println("4. update city");
+		System.out.println("5. update state");
+		System.out.println("6. update email");
+		System.out.println("7. update zip");
+		System.out.println("8. update phone number");
+		int choice = sc.nextInt();
+		
+		switch(choice) {
+			case 1:
+				System.out.println("Enter new first name: ");
+				String fname = sc.next();
+				bookitem.setFirstName(fname);
+				break;
+			case 2:
+				System.out.println("Enter new last name: ");
+				String lname = sc.next();
+				bookitem.setLastName(lname);
+				break;
+			case 3:
+				System.out.println("Enter new address: ");
+				String address = sc.next();
+				bookitem.setAddress(address);
+				break;
+			 case 4:
+				 System.out.println("Enter new city: ");
+				 String city = sc.next();
+				 bookitem.setCity(city);
+	             break;
+			 case 5:
+				 System.out.println("Enter new State: ");
+				 String state = sc.next();
+				 bookitem.setState(state);
+				 break;
+			 case 6:
+				 System.out.println("Enter new Email: ");
+				 String email = sc.next();
+				 bookitem.setEmail(email);
+				 break;
+			 case 7:
+				 System.out.println("Enter new ZipCode: ");
+				 int zip = sc.nextInt();
+				 bookitem.setZip(zip);
+				 break;
+			 case 8:
+				 System.out.println("Enter new phone number: ");
+				 int phonnum = sc.nextInt();
+				 bookitem.setPhoneNumber(phonnum);
+				 break;
+		}
+		System.out.println("Updated details = "+bookitem);
 	}
 }
